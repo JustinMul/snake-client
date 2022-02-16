62af61b8de94e39f193494344d09cb92d0305cb3
Justin Mulroney <your@email.comjustinmulroney@gmail.com>
2022-02-16 16:17:08 -0500

    adding input features

--- input.js
+++ input.js
@@ -18,12 +18,20 @@ const handleUserInput = function(data) {
   }
   if (data === 'w') {
     connection.write("Move: up");
+    connection.write('Say: up');
   } else if (data === 'a') {
     connection.write("Move: left");
+    connection.write('Say: left');
   } else if (data === 's') {
     connection.write("Move: down");
+    connection.write('Say: down');
   } else if (data === 'd') {
     connection.write("Move: right");
+    connection.write('Say: right');
+  } else if (data === 'z') {
+    connection.write('Say: Hello I am a snake ');
+  } else if (data === 'x') {
+    connection.write('Say: I am speed');
   }
 };
 
